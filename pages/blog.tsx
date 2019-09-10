d16bbc880eb9f344980d20a27844f56d74bbc88f
Marcelo Reis <marcelo.ag.reis@gmail.com>
2019-09-10 12:01:58 -0300

    Mudanças no redirect e posts

--- pages/blog.tsx
+++ pages/blog.tsx
@@ -5,11 +5,16 @@ const cache = [];
 function importAll(r: any) {
   r.keys().forEach((key: any) => cache.push(r(key)));
 }
-
 importAll(require.context("../content/", true, /\.md$/));
 
+cache.sort((a, b) => {
+  console.log("sorting");
+  if (a.attributes.date < b.attributes.date) return 1;
+  if (a.attributes.date > b.attributes.date) return -1;
+  return 0;
+});
+
 export default function Blog() {
-  console.log("cache", cache);
   return (
     <>
       {cache.map((post: any) => {
