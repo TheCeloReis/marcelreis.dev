0fefd4fd91c5e0d412e8003b5c7eda0d67e4bb95
Marcelo Reis <marcelo.ag.reis@gmail.com>
2019-09-26 11:39:13 -0300

    Configuring webpack

--- utils/analytics.tsx
+++ utils/analytics.tsx
@@ -1,8 +1,8 @@
 import ReactGA from "react-ga";
 
 export const initGA = () => {
-  if (process.env.NODE_ENV === "production") {
-    console.log("GA init");
+  if (process.env.NODE_ENV === "development") {
+    console.log("GA init", process.env.GA_TOKEN);
     ReactGA.initialize(process.env.GA_TOKEN);
   } else {
     console.log("GA init in development");
@@ -10,20 +10,20 @@ export const initGA = () => {
 };
 export const logPageView = () => {
   console.log(`Logging pageview for ${window.location.pathname}`);
-  if (process.env.NODE_ENV === "production") {
+  if (process.env.NODE_ENV === "development") {
     ReactGA.set({ page: window.location.pathname });
     ReactGA.pageview(window.location.pathname);
   }
 };
 export const logEvent = (category = "", action = "") => {
   console.log(`Logging event for ${category}/${action}`);
-  if (process.env.NODE_ENV === "production" && (category && action)) {
+  if (process.env.NODE_ENV === "development" && (category && action)) {
     ReactGA.event({ category, action });
   }
 };
 export const logException = (description = "", fatal = false) => {
   console.log(`Logging exception for ${description}/${fatal ? "FATAL" : ""}`);
-  if (process.env.NODE_ENV === "production" && description) {
+  if (process.env.NODE_ENV === "development" && description) {
     ReactGA.exception({ description, fatal });
   }
 };
