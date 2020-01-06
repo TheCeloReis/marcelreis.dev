386b16349105830131ae4c330fe87d48226b10af
marcelreis <oi@marcelreis.dev>
2020-01-05 21:34:00 -0300

    feat: webpack markdown loader

--- next.config.js
+++ next.config.js
@@ -1,21 +1,10 @@
-const webpack = require("webpack");
-const dotenv = require("dotenv");
-dotenv.config();
-
 module.exports = {
 	webpack: config => {
-		const env = Object.keys(process.env).reduce((acc, curr) => {
-			acc[`process.env.${curr}`] = JSON.stringify(process.env[curr]);
-			return acc;
-		}, {});
+		config.module.rules.push({
+			test: /\.md$/,
+			use: "frontmatter-markdown-loader"
+		});
 
-		config.plugins.push(new webpack.DefinePlugin(env));
 		return config;
-	},
-
-	exportPathMap: function() {
-		return {
-			"/": { page: "/" }
-		};
 	}
 };
