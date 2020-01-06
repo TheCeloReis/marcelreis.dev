27b353e766111cde04b615430b3597db152a9cb2
marcelreis <oi@marcelreis.dev>
2020-01-05 22:04:00 -0300

    fix: webpack development

--- next.config.js
+++ next.config.js
@@ -1,9 +1,20 @@
+const webpack = require("webpack");
+const dotenv = require("dotenv");
+dotenv.config();
+
 module.exports = {
 	webpack: config => {
 		config.module.rules.push({
 			test: /\.md$/,
 			use: "frontmatter-markdown-loader"
 		});
+
+		const env = Object.keys(process.env).reduce((acc, curr) => {
+			acc[`process.env.${curr}`] = JSON.stringify(process.env[curr]);
+			return acc;
+		}, {});
+
+		config.plugins.push(new webpack.DefinePlugin(env));
 
 		return config;
 	}
