1e995bb8b33e616e00119d69e5cb87497fa36f55
marcelreis <oi@marcelreis.dev>
2020-01-08 16:27:44 -0300

    feat: update project type

--- types/projetc.ts
+++ types/projetc.ts
@@ -2,9 +2,11 @@ export type ProjectType = {
 	attributes: {
 		title: string;
 		url: string;
+		github: string;
 		website: string;
 		description: string;
 		thumbnail: string;
+		hightlight: "0" | "1" | "2" | "3";
 	};
 	html: string;
 };
