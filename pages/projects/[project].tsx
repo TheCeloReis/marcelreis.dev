848dfc44e534f43bd3ec44d904b40137e44306e7
marcelreis <oi@marcelreis.dev>
2020-01-08 18:58:47 -0300

    feat: Redirect component

--- pages/projects/[project].tsx
+++ pages/projects/[project].tsx
@@ -1,23 +1,36 @@
 import React from "react";
 import { NextPageContext } from "next";
+import { getProjects } from "../../utils/getContent";
+import { ProjectType } from "../../types/projetc";
 
 import Router from "next/router";
 
 type PropsType = {
-	project: string;
+	project: ProjectType | undefined;
+	query: string;
 };
 
 const Post = (props: PropsType) => {
-	if (!props.project) {
+	console.log("props :", props);
+
+	if (!props.project && process.browser) {
 		Router.push("/projects");
 	}
 
-	return <p>Post: {props.project}</p>;
+	if (!props.project) {
+		return <></>;
+	}
+
+	return <p>Post: {props.project.attributes.title}</p>;
 };
 
 Post.getInitialProps = (context: NextPageContext) => {
+	const project = getProjects().find(
+		project => project.attributes.url === context.query.project
+	);
+
 	return {
-		project: context.query.project
+		project: project
 	};
 };
 
