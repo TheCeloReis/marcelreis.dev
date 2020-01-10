4c28603675d722dc00dc0246dd0ee51da3c24247
marcelreis <oi@marcelreis.dev>
2020-01-10 03:03:17 -0300

    fix: workaround to fix not processing env variable

--- utils/analytics.tsx
+++ utils/analytics.tsx
@@ -1,7 +1,15 @@
 import ReactGA from "react-ga";
 
+// NODE_ENV IS NOT SET AS DEVELOPMENT DURING THE EXPORT PROCESS, NEEDS INVESTIGATION
+const isProduction = () =>
+	!(
+		process.env.NODE_ENV === "development" ||
+		window.location.href.includes("localhost") ||
+		window.location.href.includes("netlify.com")
+	);
+
 export const initGA = () => {
-	if (process.env.NODE_ENV === "production") {
+	if (isProduction()) {
 		console.log("GA init");
 		ReactGA.initialize(process.env.GA_TOKEN || "");
 	} else {
@@ -10,20 +18,20 @@ export const initGA = () => {
 };
 export const logPageView = () => {
 	console.log(`Logging pageview for ${window.location.pathname}`);
-	if (process.env.NODE_ENV === "production") {
+	if (isProduction()) {
 		ReactGA.set({ page: window.location.pathname });
 		ReactGA.pageview(window.location.pathname);
 	}
 };
 export const logEvent = (category = "", action = "") => {
 	console.log(`Logging event for ${category}/${action}`);
-	if (process.env.NODE_ENV === "production" && category && action) {
+	if (isProduction() && category && action) {
 		ReactGA.event({ category, action });
 	}
 };
 export const logException = (description = "", fatal = false) => {
 	console.log(`Logging exception for ${description}/${fatal ? "FATAL" : ""}`);
-	if (process.env.NODE_ENV === "production" && description) {
+	if (isProduction() && description) {
 		ReactGA.exception({ description, fatal });
 	}
 };
