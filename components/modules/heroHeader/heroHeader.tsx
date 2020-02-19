92028451aa889dfbba3adc4424db2568ea5cff97
Marcelo Reis <marcelo.ag.reis@gmail.com>
2020-02-18 23:29:02 -0300

    feat: custom hero header

--- components/modules/heroHeader/heroHeader.tsx
+++ components/modules/heroHeader/heroHeader.tsx
@@ -3,12 +3,15 @@ import React, { ReactChild } from "react";
 import * as S from "./styled";
 
 type PropsType = {
+  height?: string;
+  backgroundImage?: string;
+  backgroundColor?: string;
   children: ReactChild;
 };
 
 export default function HeroHeader(props: PropsType) {
   return (
-    <S.Background>
+    <S.Background height={props.height}>
       <S.Header>{props.children}</S.Header>
     </S.Background>
   );
--- components/modules/heroHeader/styled.tsx
+++ components/modules/heroHeader/styled.tsx
@@ -1,21 +1,19 @@
 import styled from "styled-components";
 
-export const Background = styled.div`
+export const Background = styled.div<{ height?: string }>`
   width: 100%;
   display: flex;
   justify-content: center;
   align-items: center;
   padding: 8px;
 
-  height: calc(100vh - 4rem);
+  height: calc(${({ height }) => height || 100}vh - 64px);
   background-color: #171717;
 `;
 
 export const Header = styled.div`
   ${({ theme }) => theme.media.greaterThan.medium} {
-    font-size: 3rem;
     max-width: 768px;
   }
-
   text-align: center;
 `;
