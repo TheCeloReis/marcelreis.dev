d5bbc5ae5f24df309f9424ff784f4580e3c87d03
Marcelo Reis <marcelo.ag.reis@gmail.com>
2020-03-07 01:54:27 -0300

    slider button adjust

--- components/base/buttons/sliderButton/styled.tsx
+++ components/base/buttons/sliderButton/styled.tsx
@@ -3,9 +3,9 @@ import { Button } from "../button";
 
 export const StyledButton = styled(Button)<{ active: boolean }>`
   width: 48px;
-  background-color: ${({ theme }) => theme.colors.main[3]};
+  background-color: ${({ theme }) =>
+    theme.darkTheme ? theme.colors.main[4] : theme.colors.main[3]};
   border-radius: 4px;
-  box-shadow: ${({ theme }) => theme.boxShadow[1]};
 
   > div {
     width: 32px;
