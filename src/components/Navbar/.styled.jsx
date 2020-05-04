2f1c8e8385abf7c93694dcfdae8e08ddd5fae7de
MarcelReis <marcelo.ag.reis@gmail.com>
2020-05-03 23:55:50 -0300

    Initial styled-components implementation

---
+++ src/components/Navbar/.styled.jsx
@@ -0,0 +1,6 @@
+import styled from "styled-components";
+
+export const NavBar = styled.nav`
+  width: 100%;
+  height: 50px;
+`;
---
+++ src/components/Navbar/index.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+import * as S from "./.styled";
+
+const Navbar = () => {
+  return <S.NavBar>Navbar</S.NavBar>;
+};
+
+export default Navbar;
