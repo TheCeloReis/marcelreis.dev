1ffd284ed7b4567dc562b9c1aded01ac8386e5ae
Marcelo Reis <oi@marcelreis.dev>
2020-05-14 01:13:14 -0300

    melhoria do header

--- src/components/TypedHeader/index.jsx
+++ src/components/TypedHeader/index.jsx
@@ -1,38 +1,16 @@
-import React, { useEffect, useRef } from "react";
-import Typed from "typed.js";
+import React from "react";
 
 import * as S from "./styled";
 
-var options = {
-  strings: ["<i>First</i> sentence.", "&amp; a second sentence."],
-  typeSpeed: 40,
-};
-
 const TypedHeader = () => {
-  const typedRef = useRef(null);
-
-  useEffect(() => {
-    new Typed(typedRef.current, {
-      strings: [
-        "Front end developer",
-        "WebDesigner Enthusiast",
-        "Explaning code with words",
-      ],
-      typeSpeed: 30,
-      fadeOut: true,
-      fadeOutDelay: 2000,
-      loop: true,
-      loopCount: Infinity,
-      startDelay: 1000,
-      backDelay: 2000,
-    });
-  }, []);
-
   return (
     <S.Container>
-      <S.FirstLine>Hello, I'm Marcelo Reis</S.FirstLine>
-      <br />
-      <S.SecondLine ref={typedRef}></S.SecondLine>
+      <S.FirstLine>I'm Marcelo Reis</S.FirstLine>
+      <S.Lines>
+        <S.FadeDownLine>Front-end Developer</S.FadeDownLine>
+        <S.FadeDownLine>Web Designer Enthusiast</S.FadeDownLine>
+        <S.FadeDownLine>Explaining code with words</S.FadeDownLine>
+      </S.Lines>
     </S.Container>
   );
 };
--- src/components/TypedHeader/styled.js
+++ src/components/TypedHeader/styled.js
@@ -1,19 +1,67 @@
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
 
 export const Container = styled.div`
   position: absolute;
-  top: 40%;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
   width: 100%;
-  height: 0px;
 
   color: var(--white);
   text-align: center;
 
-  font-size: var(--heading-3);
+  font-size: var(--heading-1);
 `;
 
-export const FirstLine = styled.span`
+export const FirstLine = styled.h1`
+  font-size: var(--heading-2);
+
+  @media screen and (min-width: 576px) {
+    font-size: var(--heading-1);
+  }
+`;
+
+const fadeDown = keyframes`
+  75% {
+    transform: translate(-50%, -100%);
+    opacity: 0;
+  }
+  95% {
+    transform: translate(-50%, 0%);
+    opacity: 1;
+  }
+  100% {
+    transform: translate(-50%, 0%) scale(0.9);
+    opacity: 0;
+  }
+`;
+
+export const Lines = styled.div`
+  position: relative;
+  margin-top: var(--spacing-2);
   font-size: var(--heading-4);
+
+  @media screen and (min-width: 576px) {
+    font-size: var(--heading-2);
+  }
 `;
 
-export const SecondLine = styled.span``;
+export const FadeDownLine = styled.p`
+  position: absolute;
+  left: 50%;
+  width: 100%;
+  transform: translate(-50%, -100%);
+  opacity: 0;
+
+  animation: 12s ${fadeDown} ease 0s infinite;
+
+  :nth-of-type(1) {
+    animation-delay: -8s;
+  }
+  :nth-of-type(2) {
+    animation-delay: -4s;
+  }
+  :nth-of-type(3) {
+    animation-delay: 0s;
+  }
+`;
