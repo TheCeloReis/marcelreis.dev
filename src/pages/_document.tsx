fecef4d83c7d2227ca5d60225015765a1a423aa9
Marcelo Reis <oi@marcelreis.dev>
2020-05-17 16:50:20 -0300

    chore: add styled components

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,11 +5,19 @@ import Document, {
   NextScript,
   DocumentContext,
 } from "next/document";
+import { ServerStyleSheet } from "styled-components";
 
 class MyDocument extends Document {
-  static async getInitialProps(ctx: DocumentContext) {
-    const initialProps = await Document.getInitialProps(ctx);
-    return { ...initialProps };
+  static async getInitialProps({ renderPage }: DocumentContext) {
+    const sheet = new ServerStyleSheet();
+
+    const page = renderPage((App) => (props) =>
+      sheet.collectStyles(<App {...props} />)
+    );
+
+    const styleTags = sheet.getStyleElement();
+
+    return { ...page, styleTags };
   }
 
   render() {
