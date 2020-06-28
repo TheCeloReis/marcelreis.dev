48965e2a20156cec71091006d56c6a0a0319c94c
Marcelo Reis <oi@marcelreis.dev>
2020-06-28 13:21:12 -0300

    feat: labs cards improvements

--- src/components/projectsShowcase/index.tsx
+++ src/components/projectsShowcase/index.tsx
@@ -1,4 +1,6 @@
 import React from "react";
+import { FaGithub } from "react-icons/fa";
+import { FiGlobe } from "react-icons/fi";
 
 import styles from "./.module.scss";
 
@@ -14,16 +16,31 @@ const ProjectShowcase = ({ projects }: PropsType) => {
   return (
     <div className={styles.container}>
       {projects.map((project) => (
-        <Card key={project.url}>
-          <h2 className={styles.title}>{project.title}</h2>
+        <Card className={styles.card} key={project.url}>
+          <header className={styles.header}>
+            <h2 className={styles.title}>{project.title}</h2>
+            {project.github && (
+              <div>
+                <a className={styles.icon} href={project.github}>
+                  <FaGithub />
+                </a>
+              </div>
+            )}
+
+            {project.website && (
+              <div>
+                <a className={styles.icon} href={project.website}>
+                  <FiGlobe />
+                </a>
+              </div>
+            )}
+          </header>
           <p>{project.description}</p>
           <ChipContainer>
             {project.stack.map((tech) => (
               <Chip key={tech}>{tech}</Chip>
             ))}
           </ChipContainer>
-          {project.github && <div>{project.github}</div>}
-          {project.website && <div>{project.website}</div>}
         </Card>
       ))}
     </div>
