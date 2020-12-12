0612d93e94c24bc57856f039582a64e3ce093a66
Marcelo Reis <oi@marcelreis.dev>
2020-12-12 20:51:23 -0300

    fix: header desktop size

--- packages/marcelreis.dev/src/components/HeroHeader/HeroHeader.styled.tsx
+++ packages/marcelreis.dev/src/components/HeroHeader/HeroHeader.styled.tsx
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import media from "styled-media-query";
 
 export const Container = styled.header`
   position: absolute;
@@ -11,9 +12,12 @@ export const Container = styled.header`
 
 export const H1 = styled.h1`
   color: #fff;
-  font-size: 2rem;
+  font-size: 2.75rem;
   font-weight: bold;
   white-space: nowrap;
+  ${media.greaterThan("medium")`
+    font-size: 4rem;
+  `}
 `;
 
 export const P = styled.p`
@@ -21,4 +25,7 @@ export const P = styled.p`
   font-size: 1.5rem;
   white-space: nowrap;
   margin-top: 0.75rem;
+  ${media.greaterThan("medium")`
+    font-size: 2rem;
+  `}
 `;
